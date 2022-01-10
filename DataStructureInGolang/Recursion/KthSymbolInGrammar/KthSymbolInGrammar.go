b1ead066adcb1fd954b63ce5bd1fe8f8e8d0c451
VibhorDubey <vibhordubey333@gmail.com>
2022-01-10 23:25:51 +0530

    DataStructureInGolang|Recursion|KthSymbolInGrammar

---
+++ DataStructureInGolang/Recursion/KthSymbolInGrammar/KthSymbolInGrammar.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	n, k := 2, 2
+	fmt.Println(KthSymbolInGrammar(n, k))
+}
+
+func KthSymbolInGrammar(n, k int) int {
+	if n == 1 && k == 1 {
+		return 0
+	}
+	mid := int(math.Pow(2, float64((n-1))) / 2)
+	if k <= mid {
+		return KthSymbolInGrammar(n-1, k)
+	} else {
+		return 1 ^ KthSymbolInGrammar(n-1, k-mid)
+	}
+}
