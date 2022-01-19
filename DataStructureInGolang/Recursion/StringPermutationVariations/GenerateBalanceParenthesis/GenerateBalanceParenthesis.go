3b61c9a882af69e3e84c436efcdbb9556980fad0
Vibhor Dubey <vibhor.dubey@hpe.com>
2022-01-19 17:11:01 +0530

    DataStrucutureUsingGolang|Recursion|GenerateAllBalanceParenthesis

---
+++ DataStructureInGolang/Recursion/StringPermutationVariations/GenerateBalanceParenthesis/GenerateBalanceParenthesis.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	userInput := 3
+	open := userInput
+	close := userInput
+	output := ""
+	generateAllBalanceParenthesis(open, close, output)
+}
+
+func generateAllBalanceParenthesis(open, close int, output string) {
+	if open == 0 && close == 0 {
+		fmt.Print(output, " ")
+		return
+	}
+	if open != 0 {
+		output1 := output
+		output1 += "("
+		generateAllBalanceParenthesis(open-1, close, output1)
+	}
+	if close > open {
+		output2 := output
+		output2 += ")"
+		generateAllBalanceParenthesis(open, close-1, output2)
+	}
+}
