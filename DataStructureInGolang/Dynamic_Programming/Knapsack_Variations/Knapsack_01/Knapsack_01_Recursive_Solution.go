60b6841949edf860a01bdf4b9c77b35ef01bfe6b
VibhorDubey <vibhordubey333@gmail.com>
2022-01-24 23:49:33 +0530

    DataStructureInGolang|DynamicProgramming|Knapsack_01_Recursive-Added comments

--- DataStructureInGolang/Dynamic_Programming/Knapsack_Variations/Knapsack_01/Knapsack_01_Recursive_Solution.go
+++ DataStructureInGolang/Dynamic_Programming/Knapsack_Variations/Knapsack_01/Knapsack_01_Recursive_Solution.go
@@ -20,13 +20,17 @@ func main() {
 }
 
 func knapsack(weight, value []int, W, N int) int {
+	/*
+		Lowest valid input for N is 0 as there can be zero elements left to put in a bag.
+		W is 0, say in input is given that knapsack can carry 0 amount of weight.
+	*/
 	if N == 0 || W == 0 {
 		return 0
 	}
 	//Happy case.
 	if weight[N-1] <= W {
 		return int(math.Max(float64(value[N-1]+knapsack(weight, value, W-weight[N-1], N-1)), float64(knapsack(weight, value, W, N-1))))
-	} else if weight[N-1] > W {
+	} else if weight[N-1] > W { // Non-happy scenario.
 		return knapsack(weight, value, W, N-1)
 	}
 	return 1
