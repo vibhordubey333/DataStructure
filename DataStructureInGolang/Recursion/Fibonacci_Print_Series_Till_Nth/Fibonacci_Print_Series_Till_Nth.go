9a67d97af523253dcb06377cbb57da8661c23553
Vibhor Dubey <vibhordubey333@gmail.com>
2022-06-17 18:55:01 +0530

    Update Fibonacci_Print_Series_Till_Nth.go

--- DataStructureInGolang/Recursion/Fibonacci_Print_Series_Till_Nth/Fibonacci_Print_Series_Till_Nth.go
+++ DataStructureInGolang/Recursion/Fibonacci_Print_Series_Till_Nth/Fibonacci_Print_Series_Till_Nth.go
@@ -7,6 +7,8 @@ import (
 /*Notes:
 1. If while calling printFibonacci we do n-2 then pattern for N=5 will be [0,1,1,2,3]. Comment this line below fmt.Printf(" 0 || 1 ||")
 2. And if we pass N=5 as it is then output will be [1,2,3,5,8]
+3. Sample Fibonacci till 15
+	0 || 1 ||1 || 2 || 3 || 5 || 8 || 13 || 21 || 34 || 55 || 89 || 144 || 233 || 377 || 610 || 987
 */
 
 func main() {
