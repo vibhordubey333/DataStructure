a8679c9d92ac16622d81821295000c6fd4a288ea
Vibhor Dubey <vibhordubey333@gmail.com>
2022-06-21 23:02:31 +0530

    Update KthSymbolInGrammar.go

--- DataStructureInGolang/Recursion/KthSymbolInGrammar/KthSymbolInGrammar.go
+++ DataStructureInGolang/Recursion/KthSymbolInGrammar/KthSymbolInGrammar.go
@@ -1,23 +1,33 @@
+// You can edit this code!
+// Click here and start typing.
 package main
 
 import (
 	"fmt"
 	"math"
 )
+/*
+0
+01
+0110
+0110 1001
+01101001 10010110
+*/
 
 func main() {
 	n, k := 2, 2
-	fmt.Println(KthSymbolInGrammar(n, k))
+	fmt.Println("Digit present at ", k, " position is :", kthSymbolInGrammar(n, k))
 }
 
-func KthSymbolInGrammar(n, k int) int {
+func kthSymbolInGrammar(n, k int) int {
 	if n == 1 && k == 1 {
 		return 0
 	}
-	mid := int(math.Pow(2, float64((n-1))) / 2)
+	//Computing mid and then we'll compare them with k whether it is lt or gt
+	mid := int(math.Pow(2, float64(n-1)) / 2)
 	if k <= mid {
-		return KthSymbolInGrammar(n-1, k)
+		return kthSymbolInGrammar(n-1, k)
 	} else {
-		return 1 ^ KthSymbolInGrammar(n-1, k-mid) // It will give complement of first half.
+		return 1 ^ kthSymbolInGrammar(n-1, k-mid)
 	}
 }
