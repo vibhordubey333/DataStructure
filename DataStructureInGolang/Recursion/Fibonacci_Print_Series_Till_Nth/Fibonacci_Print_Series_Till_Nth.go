3d48d628e9972360fbceeea624bb0507048ad720
Vibhor Dubey <vibhordubey333@gmail.com>
2022-08-29 20:50:33 +0530

    DataStructure|Golang|Recursion|Print fibonacci series

--- DataStructureInGolang/Recursion/Fibonacci_Print_Series_Till_Nth/Fibonacci_Print_Series_Till_Nth.go
+++ DataStructureInGolang/Recursion/Fibonacci_Print_Series_Till_Nth/Fibonacci_Print_Series_Till_Nth.go
@@ -1,8 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+/*Sample Fibonacci till 15
+0 || 1 ||1 || 2 || 3 || 5 || 8 || 13 || 21 || 34 || 55 || 89 || 144 || 233 || 377 || 610 || 987
+*/
+func main() {
+	n := 5
+	if n <= 0 {
+		fmt.Println("Enter positive number")
+	}
+	for i := 0; i <= n; i++ {
+		fmt.Print(" ",Fibonacci(i)," ,")
+	}
+
+}
+//Common functionality to print series of fibonacci series and Nth term of fibonacci.
+func Fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
+
+//Ignore
 
 /*Notes:
 1. If while calling printFibonacci we do n-2 then pattern for N=5 will be [0,1,1,2,3]. Comment this line below fmt.Printf(" 0 || 1 ||")
@@ -10,7 +31,7 @@ import (
 3. Sample Fibonacci till 15
 	0 || 1 ||1 || 2 || 3 || 5 || 8 || 13 || 21 || 34 || 55 || 89 || 144 || 233 || 377 || 610 || 987
 */
-
+/*
 func main() {
 	firstNum := 0
 	secondNum := 1
@@ -28,3 +49,4 @@ func printFibonacci(firstNum, secondNum, n int) {
 		printFibonacci(firstNum, secondNum, n-1)
 	}
 }
+*/
