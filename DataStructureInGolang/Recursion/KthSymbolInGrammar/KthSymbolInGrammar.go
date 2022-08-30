ceeeff0ef371c1cea1dcfd091760b89da4230281
Vibhor Dubey <vibhor.dubey@hpe.com>
2022-08-30 14:24:45 +0530

    DataStructure|Golang|Recursion|Refactored KthSymbolGrammar

--- DataStructureInGolang/Recursion/KthSymbolInGrammar/KthSymbolInGrammar.go
+++ DataStructureInGolang/Recursion/KthSymbolInGrammar/KthSymbolInGrammar.go
@@ -1,11 +1,10 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
 import (
 	"fmt"
 	"math"
 )
+
 /*
 0
 01
@@ -15,10 +14,18 @@ import (
 */
 
 func main() {
+	//N represents no. of lines and k is the index where we've to check which digit is present from 0 and 1 on the Nth line.
 	n, k := 2, 2
 	fmt.Println("Digit present at ", k, " position is :", kthSymbolInGrammar(n, k))
 }
-
+/*
+Algorithm:
+1. Base condition: If n==1 and k ==1 then return
+2. Compute mid element.
+3. If k <= mid then return self_function(n-1,k)
+   Else
+	 return 1 ^ self_function(n-1,k-mid)
+*/
 func kthSymbolInGrammar(n, k int) int {
 	if n == 1 && k == 1 {
 		return 0
@@ -28,6 +35,7 @@ func kthSymbolInGrammar(n, k int) int {
 	if k <= mid {
 		return kthSymbolInGrammar(n-1, k)
 	} else {
+		//Returning complement of first half.
 		return 1 ^ kthSymbolInGrammar(n-1, k-mid)
 	}
 }
