7033f273469b59c67058a8e9da8a31840f238826
Vibhor Dubey <vibhordubey333@gmail.com>
2022-09-25 14:50:06 +0530

    DataStructure|Golang|DP|Added Minimum Subset Sum

---
+++ DataStructureInGolang/Dynamic_Programming/Knapsack_Variations/Minimum_Subset_Sum/Minimum_SubSet_Sum_Recursive.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	arr := make([]int, 0)
+	arr = []int{1, 2, 7}
+	n := len(arr)
+	findSum(arr, n)
+}
+
+func findSum(arr []int, n int) {
+	totalSum := 0
+	calculatedSum := 0
+	for _, v := range arr {
+		totalSum += v
+	}
+	fmt.Println("Minimum SubSetSum", findMinSubSetSum(arr, n, totalSum, calculatedSum))
+}
+
+func findMinSubSetSum(arr []int, n, totalSum, calculatedSum int) int {
+
+	//If we've reached last element. Sum of one subset is sumcalculated and sum of other subset is [sumTotal-sumCalculated]. Then return absolute difference of two sums.
+	if n == 0 {
+		return int(math.Abs(float64((totalSum - calculatedSum) - calculatedSum)))
+	}
+
+	return int(math.Min(float64(findMinSubSetSum(arr, n-1, totalSum, calculatedSum+arr[n-1])), float64(findMinSubSetSum(arr, n-1, totalSum, calculatedSum))))
+}
