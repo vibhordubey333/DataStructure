6dd10ee59491f66ddcb68b6a6fd372034144943f
Vibhor Dubey <vibhor.dubey@hpe.com>
2022-11-09 19:31:27 +0530

    DataStructureInGolang|DP|Added UnboundedKnapsack recursive approach

---
+++ DataStructureInGolang/Dynamic_Programming/3.UnBounded-Knapsack/0.UnBounded-Knapsack/UnBounded-Knapsack-Recursion.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	val := make([]int, 0)
+	wt := make([]int, 0)
+	w := 100
+	val = []int{25, 30, 15}
+	wt = []int{15, 5, 10}
+	n := 3
+	fmt.Println("Unbounded Knapsack: ", unboundedKnapsackRecursive(wt, val, w, n))
+}
+
+func unboundedKnapsackRecursive(wt, val []int, w, n int) int {
+
+	if n == 0 || w == 0 {
+		return 0
+	}
+
+	// If value is greater than the given weight don't process
+	if wt[n-1] > w {
+		return unboundedKnapsackRecursive(wt, val, w, n-1)
+	} else {
+		//1. nth item will be included.
+		//2. nth item will not be included.
+		return max(val[n-1]+unboundedKnapsackRecursive(wt, val, w-wt[n-1], n), unboundedKnapsackRecursive(wt, val, w, n-1))
+	}
+
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+
