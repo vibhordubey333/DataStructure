212563d2968dd319397cff6163e6de7cd4330e7c
Vibhor Dubey <vibhordubey333@gmail.com>
2023-01-15 10:08:58 +0530

    Update singly-linked-list.go

--- DataStructureInGolang/LinkedList/SinglyLinkedList/singly-linked-list.go
+++ DataStructureInGolang/LinkedList/SinglyLinkedList/singly-linked-list.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 type SinglyLinkedListMethods interface {
-	Insert(interface{})
-	Search(interface{})
-	Size() int
+	Insert(userInput interface{})
 	Display()
+	Size() int
+	Remove(userInput interface{})
+	Search(userInput interface{})
 	Reverse()
 }
 
@@ -14,101 +15,113 @@ type SinglyLinkedList struct {
 	head *Node
 	size int
 }
-
 type Node struct {
 	value interface{}
 	next  *Node
 }
 
 func (s *SinglyLinkedList) Insert(value interface{}) {
-	fmt.Println("Inserting element:", value)
-	dataObject := &Node{value: value, next: nil}
+	payload := &Node{value: value, next: nil}
 
 	if s.head == nil {
-		fmt.Println("Inserting in front as list is nil")
-		s.head = dataObject
+		s.head = payload
 		s.size++
-	} else {
-		fmt.Println("Inserting in rear as one or more element is present")
-		ptr := s.head
-		for i := 0; i < s.size; i++ {
-			if ptr.next == nil {
-				ptr.next = dataObject
-				s.size++
-				return
-			}
-			ptr = ptr.next
-		}
+		fmt.Println("Inserted: ", value, " to empty linked list successfully.")
+		return
 	}
 
+	ptr := s.head
+
+	for ptr.next != nil {
+		ptr = ptr.next
+	}
+	ptr.next = payload
+	s.size++
+	fmt.Println("Inserted:", value, " at the end of the linked list.")
 }
 
-func (s *SinglyLinkedList) Search(value interface{}) {
-	if s.Size() > 0{
-		ptr := s.head
-		locationPtr := 0
-		for ptr != nil{
-			if ptr.value == value{
-				fmt.Println("Element ",ptr.value," found at index, ",locationPtr)
+func (s *SinglyLinkedList) Display() {
+	ptr := s.head
+	for ptr != nil {
+		fmt.Print(ptr.value, " -> ")
+		ptr = ptr.next
+	}
+	fmt.Print(" X ")
+}
+
+func (s *SinglyLinkedList) Search(userInput interface{}) {
+	ptr := s.head
+
+	if ptr != nil {
+		for ptr != nil {
+			if ptr.value == userInput {
+				fmt.Println("Element found at: ", ptr.value)
 				return
 			}
 			ptr = ptr.next
-			locationPtr++
 		}
-	}else{
-		fmt.Println("No elements in list to search.")
+	} else {
+		fmt.Println("List is empty.")
+		return
 	}
-	fmt.Println("Element ",value," not present in list.")
+	fmt.Println("Element not found")
+}
+
+func (s *SinglyLinkedList) Reverse() {
+	if s.head != nil {
+		var next, prev *Node
+		curr := s.head
+
+		for curr != nil {
+			next = curr.next
+			curr.next = prev
+			prev = curr
+			curr = next
+		}
+		s.head = prev
+		s.Display()
+		return
+	}
+
+	fmt.Println("No items present in list.")
+}
+
+func (s *SinglyLinkedList) Remove(userInput interface{}) {
+
+	if s.head != nil {
+		ptr := s.head
+		for ptr.next != nil {
+			if ptr.next.value == userInput {
+				ptr.next = ptr.next.next
+				fmt.Println("Element Removed")
+				s.size--
+				return
+			}
+			ptr = ptr.next
+		}
+
+	} else {
+		fmt.Println("List is empty.")
+	}
+
 }
 
 func (s *SinglyLinkedList) Size() int {
 	return s.size
 }
 
-func (s *SinglyLinkedList) Display() {
-	if s.Size() > 0 {
-		ptr := s.head
-		for ptr != nil{
-			fmt.Println("Element: ",ptr.value)
-			ptr = ptr.next
-		}
-	} else {
-		fmt.Println("LinkedList is empty.")
-	}
-}
-
-func (s *SinglyLinkedList) Reverse() {
-
-	curr := s.head
-	var prev *Node
-	var next *Node
-
-	for curr != nil {
-		next = curr.next
-		curr.next = prev
-		prev = curr
-		curr = next
-	}
-	s.head = prev
-}
-
 func main() {
-	var llo SinglyLinkedListMethods
-	llo = new(SinglyLinkedList)
-	llo.Insert("A")
-	llo.Insert("B")
-	llo.Insert("C")
-	llo.Insert("D")
-	fmt.Println("SinglyLinkedList Size:", llo.Size())
-	fmt.Println("Before Reversing: ")
-	llo.Display()
-	fmt.Println("After Reversing List:")
-	llo.Reverse()
-	llo.Display()
-	fmt.Print("Searching element A in list: ")
-	llo.Search("A")
-	fmt.Print("Searching element C in list: ")
-	llo.Search("C")
-	fmt.Print("Searching element ERT in list: ")
-	llo.Search("ERT")
+	var interfaceObject SinglyLinkedListMethods
+	interfaceObject = new(SinglyLinkedList)
+	interfaceObject.Insert("A")
+	interfaceObject.Insert("B")
+	interfaceObject.Insert("C")
+	fmt.Println("Size is :", interfaceObject.Size())
+	interfaceObject.Display()
+	fmt.Println()
+	interfaceObject.Search("AN")
+	interfaceObject.Reverse()
+	interfaceObject.Remove("A")
+	fmt.Println("After Removing element")
+	interfaceObject.Display()
 }
