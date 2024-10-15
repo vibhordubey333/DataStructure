048b6df49520f26f4fe6a17e1b49fdd374848172
Vibhor Dubey <vibhordubey333@gmail.com>
2024-10-15 20:19:50 +0530

    Update 88-Merge-Sorted-Array-LC[150]-Easy.go

--- DataStructureInGolang/LeetCode/Arrays/88-Merge-Sorted-Array-LC[150]-Easy.go
+++ DataStructureInGolang/LeetCode/Arrays/88-Merge-Sorted-Array-LC[150]-Easy.go
@@ -12,14 +12,10 @@ import(
     "sort"
 )
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-   
-   for i,j := 0,m ; i < n ; i++{
-        nums1[j] = nums2[i]
-        j++
+   for i := 0 ; i < n ; i++{
+        nums1[m+i] = nums2[i]
     }
-    
     sort.Ints(nums1)
-
 }
 
 //Three Pointer Approach.
